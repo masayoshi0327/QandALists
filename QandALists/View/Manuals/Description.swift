702b871c35aafb8150e139bf6604d22a123d01bb
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-02-15 20:32:55 +0900

    説明書テンプレート作成

---
+++ QandALists/View/Manuals/Description.swift
@@ -0,0 +1,36 @@
+//
+//  Description.swift
+//  QandALists
+//
+//  Created by 澤崎正佳 on 2021/02/15.
+//
+
+import SwiftUI
+
+struct Description: View {
+    
+    let imageNum: String
+    let description: String
+    
+    var body: some View {
+        HStack{
+        
+            Image("HowToUse\(imageNum)")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 150)
+
+            
+            Spacer()
+            
+            Text(description)
+        }
+        .frame(width: 300)
+    }
+}
+
+struct Description_Previews: PreviewProvider {
+    static var previews: some View {
+        Description(imageNum: "2-1", description: "右下のボタンから新規作成画面に移動できます。")
+    }
+}
