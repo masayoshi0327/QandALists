6e9488c5982263c81eb9655fd4526fc17af9474f
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-02-15 22:56:05 +0900

    説明書4ページ目

--- QandALists/View/Manuals/FourthPage.swift
+++ QandALists/View/Manuals/FourthPage.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct FourthPage: View {
     var body: some View {
-        Text("わかったことを読み返そう")
+        VStack{
+            
+            Text("わかったことを読み返そう")
+                .font(.title2)
+                .fontWeight(.bold)
+            
+            Description(imageNum: "4-1", description: "画面下のタブを切り替えるとわかったことリストに移動します。わかったことはすべてここに保存されています。")
+            
+            Description(imageNum: "4-2", description: "検索機能で特定のタイトルを探せます。また、画面右上にはリストの数が表示されています。どんどん数を増やしましょう。")
+            
+        }
     }
 }
 
